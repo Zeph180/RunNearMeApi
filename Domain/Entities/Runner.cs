e922bfeb2e658f12f01112ba2d33eaa5ffe87984
zeph180 <zephrichards1@gmail.com>
2025-07-23 23:30:40 +0300

    Create runner entity

---
+++ Domain/Entities/Runner.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities;
+
+public class Runner
+{
+    public required Guid RunnerId { get; set; }
+    public required string Name { get; set; }
+    public required string NickName { get; set; }
+    public required string Password { get; set; }
+    public required string Email { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string Address { get; set; }
+    public required string City { get; set; }
+    public required string State { get; set; }
+    public int Age { get; set; }
+    public int Height { get; set; }
+    public int Weight { get; set; }
+}
