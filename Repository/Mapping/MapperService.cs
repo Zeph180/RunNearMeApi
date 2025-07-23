229f87cfc6acffc4534a241716383d048a29cba7
zeph180 <zephrichards1@gmail.com>
2025-07-24 01:38:30 +0300

    Connect to database

---
+++ Repository/Mapping/MapperService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Repository.Mapping;
+
+public class MapperService : Profile
+{
+    public MapperService()
+    {
+        CreateMap<Runner, RunnerDto>();
+        CreateMap<RunnerDto, Runner>();
+    }
+}
