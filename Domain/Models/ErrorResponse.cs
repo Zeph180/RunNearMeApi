91672312365ab0198ec25f24b499e986dad52f68
zeph180 <zephrichards1@gmail.com>
2025-07-24 20:02:00 +0300

    Create error response

---
+++ Domain/Models/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+
+public class ErrorResponse
+{
+    public string Message { get; set; }
+    public string ErrorCode { get; set; }
+    public string StatusCode { get; set; }
+    public string TraceId { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public Dictionary<string, string> Details { get; set; }
+}
