6f117f1543d3bbef6b38a15b27679a3e66c26cfd
zeph180 <zephrichards1@gmail.com>
2025-07-24 19:49:42 +0300

    Add Business exception

---
+++ Application/Middlewares/ErrorHandling/BusinessException.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Middlewares.ErrorHandling;
+
+public class BusinessException : Exception
+{
+    public string ErrorCode { get; }
+    public string StatusCode { get; }
+
+    public BusinessException(string message, string errorCode = "BUSINESS_ERROR", string statusCode = "400") : base(message)
+    {
+        ErrorCode = errorCode;
+        StatusCode = statusCode;
+    }
+}
