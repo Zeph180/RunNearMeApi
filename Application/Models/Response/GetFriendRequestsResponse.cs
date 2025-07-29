dafa982736f70d8d944fc6424bb1c3c6031feb60
zeph180 <zephrichards1@gmail.com>
2025-07-29 20:10:59 +0300

    Create friend request response model:

---
+++ Application/Models/Response/GetFriendRequestsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Models.Response;
+
+public class GetFriendRequestResponse
+{
+    public Guid RequestId { get; set; }
+    public Guid RequesterId { get; set; }
+    public required string NickName { get; set; }
+    public required string Address { get; set; }
+}
