bfd6fab72ea574ea3b8653c6684b5b3d383235a2
emmailakut256 <emmailakut@gmail.com>
2025-07-30 22:20:19 +0300

    added group request validator

---
+++ Application/Validators/RequestValidations/Groups/GroupCreateValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models.Request.Groups;
+using Domain.Models.Request.Account;
+using FluentValidation;
+
+namespace Application.Validators.RequestValidations.Groups
+{
+    public class GroupCreateValidation : AbstractValidator<CreateGroupRequest>
+    {
+        public GroupCreateValidation() 
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .Length(2, 50).WithMessage("Name must be between 2 and 50 characters");
+
+            RuleFor(x => x.RunnerId)
+               .NotEmpty().WithMessage("RunnerId is required");
+
+            RuleFor(x => x.Description)
+               .NotEmpty().WithMessage("Group Description is required");
+        }
+    }
+}
