df064a9524f7991192639b3b42ecf0c0e884831b
emmailakut256 <emmailakut@gmail.com>
2025-07-30 22:19:09 +0300

    added groups request

---
+++ Application/Models/Request/Groups/CreateGroupRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Models.Request.Groups
+{
+    public class CreateGroupRequest
+    {
+        //public Guid GroupId { get; set; }
+        [Required]
+        public Guid RunnerId { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public required string Name { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public required string Description { get; set; }
+        //public DateTime CreatedAt { get; set; }
+        //public bool IsDeleted { get; set; }
+        //public DateTime? DeletedAt { get; set; }
+        //public ICollection<Profile>? Profiles { get; set; }
+
+    }
+}
