02765447f0210bf84082e9faeff92962220c649f
emmailakut256 <emmailakut@gmail.com>
2025-07-31 23:00:27 +0300

    created group repo

---
+++ Repository/Repositories/GroupRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.Models.Request.Groups;
+using Application.Models.Response.Groups;
+
+namespace Repository.Repositories
+{
+    public  class GroupRepository : IGroup
+    {
+        public Task<CreateGroupResponse> CreateGroup(CreateGroupRequest createGroupRequest)
+        {
+            // Implementation of group creation logic goes here
+            throw new NotImplementedException();
+        }
+
+    }
+}
