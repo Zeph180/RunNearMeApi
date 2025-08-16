08d823f120177953a2b4a7b3df94b02b14bbcb60
emmailakut256 <emmailakut@gmail.com>
2025-08-16 19:42:04 +0300

    enabled joining Challenge

---
+++ Application/Models/Response/Challenges/GetChallengesResp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Response.Challenges
+{
+    public class GetChallengesResp
+    {
+        public Guid ChallengeId { get; set; }
+        public string Name { get; set; }
+        public string Target { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Domain/Entities/ChallengeParticipant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ChallengeParticipant
+    {
+        public Guid ChallengeId { get; set; }
+        public Challenge Challenge { get; set; }
+        public Guid RunnerId { get; set; }
+        public Runner Runner { get; set; }
+        public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+        // Optional: status, progress, completion %
+        public bool Completed { get; set; } = false;
+    }
+}
