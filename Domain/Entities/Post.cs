2a4d8a79195c64d29e276339ab9be4c7d1b6fb10
zeph180 <zephrichards1@gmail.com>
2025-08-26 21:04:13 +0300

    Changes

--- Domain/Entities/Post.cs
+++ Domain/Entities/Post.cs
@@ -4,10 +4,10 @@ namespace Domain.Entities;
 
 public class Post
 {
-    public Guid PostId { get; set; }
+    public Guid PostId { get; set; } = Guid.NewGuid();
     public Guid RunnerId { get; set; }
     [MaxLength(500)]
-    public string? Caption { get; set; }
+    public string? Message { get; set; }
     public DateTime CreatedAt { get; set; }
     public bool IsDeleted { get; set; }
     public DateTime? DeletedAt { get; set; }
@@ -21,5 +21,4 @@ public class Post
     public string? Location { get; set; }
     public ICollection<Comment>? Comments { get; set; }
     public ICollection<Like>? Likes { get; set; }
-    public bool Deleted { get; set; }
 }
--- Repository/Mapping/MapperService.cs
+++ Repository/Mapping/MapperService.cs
@@ -1,8 +1,10 @@
-﻿using Application.Interfaces.Dtos;
+﻿using Application.Interfaces;
+using Application.Interfaces.Dtos;
 using Application.Interfaces.Dtos.Challenge;
 using Application.Interfaces.Dtos.Run;
 using Application.Models.Request.Authentication;
 using Application.Models.Request.Challenge;
+using Application.Models.Request.Posts;
 using Application.Models.Response.People;
 using AutoMapper;
 using Domain.Entities;
@@ -16,6 +18,14 @@ public class MapperService : Profile
 {
     public MapperService()
     {
+        CreateMap<Post, CreatePostResponse>();
+        
+        CreateMap<CreatePostRequest, Post>()
+            .ForMember(dest => dest.Message, opt => opt.MapFrom(src => src.Caption))
+            .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location))
+            .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+            .ForMember(dest => dest.RunnerId, opt => opt.MapFrom(src => src.RunnerId));
+        
         CreateMap<UpdateChallengeRequest, Challenge>();
         
         CreateMap<Challenge, ChallengeDto>()
