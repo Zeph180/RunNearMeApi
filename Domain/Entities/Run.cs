fb4f0f979fd2d5ed93355165b5135d5f4dd72101
zeph180 <zephrichards1@gmail.com>
2025-08-30 16:42:20 +0300

    updated run entity

--- Domain/Entities/Run.cs
+++ Domain/Entities/Run.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using NetTopologySuite.Geometries;
 
 namespace Domain.Entities;
 
@@ -7,25 +8,27 @@ public class Run
 {
     [Key] public Guid RunId { get; set; }
     [ForeignKey(nameof(Profile))] public Guid RunnerId { get; set; }
-
     public DateTime StartTime { get; set; } = DateTime.UtcNow;
     public DateTime? EndTime { get; set; }
-
     [Range(0, double.MaxValue)] public double DistanceInMeters { get; set; }
-
     [Range(0, double.MaxValue)] public double DurationInSeconds { get; set; }
-
     [Range(0, double.MaxValue)] public double AveragePace { get; set; }
-
     [Range(0, double.MaxValue)] public double MaxPace { get; set; }
-
     [Range(0, double.MaxValue)] public double CaloriesBurned { get; set; }
-
     [Range(0, double.MaxValue)] public double AverageHeartRate { get; set; }
-
+    
+    //NTS linestring to store the complete route 
+    public LineString? Route { get; set; }
+    
+    //The bounding box spatial queries
+    public Polygon? RouteBounds { get; set; }
+    
+    //Start and end points for quick access
+    public Point? StartPoint { get; set; }
+    public Point? EndPoint { get; set; }
+    
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
-
     public virtual Profile Profile { get; set; } = null!;
     public virtual List<RunRoutePoint> RoutePoints { get; set; } = new List<RunRoutePoint>();
 }
